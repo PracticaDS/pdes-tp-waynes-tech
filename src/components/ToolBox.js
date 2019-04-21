f2df463df75448ab70d73072e385716633ce2dbb
Mari Medici <marilau.medici@gmail.com>
2019-04-21 14:52:39 -0300

    #3 el boton que se selecciona se recuerda

--- src/components/ToolBox.js
+++ src/components/ToolBox.js
@@ -1,48 +1,72 @@
 import React, { Component } from 'react';
 import { ButtonToolBox }    from './ButtonToolBox';
+import { PropTypes }        from 'prop-types';
 import './ToolBox.css';
 
 import boton_borrar from './images/delete.PNG';
 import boton_rotar  from './images/rotate.PNG';
 import boton_move   from './images/move.PNG';
 
-import boton_crafter from './images/crafter.PNG';
-import boton_start   from './images/starter.PNG';
-import boton_furnace from './images/furnace.PNG';
-import boton_seller  from './images/seller.PNG';
+import boton_crafter     from './images/crafter.PNG';
+import boton_start       from './images/starter.PNG';
+import boton_furnace     from './images/furnace.PNG';
+import boton_seller      from './images/seller.PNG';
 import boton_transporter from './images/transporter.PNG';
 
+
 export class ToolBox extends Component {
-  
+
+
+  constructor(props) {
+    super(props);
+    this.state = { toolSeleccionado: "" };
+  }
+
+  handleSelect = (toolSeleccionado) => {
+    this.setState({ toolSeleccionado });
+  }
+
+  crearBoton(props){
+    return <ButtonToolBox 
+              seleccionado={props.nombre === this.state.toolSeleccionado}
+              onSelect={this.handleSelect}
+              nombre={props.nombre} 
+              src={props.src} 
+            /> 
+  }
+
   render() {
     return (
       <div className="ToolBox">
-        <h3>Máquinas</h3>
-        <table>
-          <tr>
-            <td><ButtonToolBox src={boton_start} /></td>
-            <td><ButtonToolBox src={boton_seller} /></td>
-          </tr>
-          <tr>
-            <td><ButtonToolBox src={boton_crafter} /></td>
-            <td><ButtonToolBox src={boton_furnace} /></td>
-          </tr>
-          <tr>
-            <td><ButtonToolBox src={boton_transporter} /></td>
-          </tr>
-        </table>
+              <h3>Máquinas</h3>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>{this.crearBoton({nombre: "starter", src: boton_start })}</td>
+                      <td>{this.crearBoton({nombre: "crafter", src: boton_crafter })}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.crearBoton({nombre: "seller", src: boton_seller })}</td>
+                      <td>{this.crearBoton({nombre: "furnace", src: boton_furnace })}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.crearBoton({nombre: "transporter", src: boton_transporter })}</td>
+                    </tr>
+                  </tbody>
+                </table>
 
-        <h3>Edición</h3>
-        <table>
-          <tr>
-            <td><ButtonToolBox src={boton_borrar} /></td>
-            <td><ButtonToolBox src={boton_move} /></td>
-          </tr>
-          <tr>
-            <td><ButtonToolBox src={boton_rotar} /></td>
-          </tr>
-        </table>
-
+                <h3>Edición</h3>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>{this.crearBoton({nombre: "delete", src: boton_borrar })}</td>
+                      <td>{this.crearBoton({nombre: "move",   src: boton_move })}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.crearBoton({nombre: "rotate",   src: boton_rotar })}</td>
+                    </tr>
+                  </tbody>
+                </table>
       </div>
     )
   }
