fe2e53773806c1a8540b054e7a9713beaca17894
Diego <garcia.diego1990@gmail.com>
2019-04-27 20:21:45 -0300

    test fabrica

--- src/components/Fabrica.test.js
+++ src/components/Fabrica.test.js
@@ -1,36 +1,43 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import {Fabrica} from './Fabrica'
 import {Fila} from './Fila'
 import './setupTests.js'
 
 describe("Fabrica", () => {
-    function crearFabrica(){
-        
-        const fabrica = shallow(<Fabrica filas = {6} columnas= {8}/>);
+    function crearFabrica(){   
+        const fabrica = mount(<Fabrica className="fabrica" filas = {6} columnas= {8}/>);
         return fabrica;
     }
 
     it("La cantidad de filas deberia ser 6", () =>{
         const fabrica = crearFabrica();
         expect (fabrica.find(Fila).length).toEqual(6);
+    })
+
+    it("La cantidad de columnas deberia ser 8", () =>{
+        const fabrica = crearFabrica();
+        const fila = fabrica.find(Fila).at(0);
+        expect (fila.prop("columnas")).toEqual(8);
+    })
+
+    it("El índice de la fila deberia ser 3",() =>{
+        const fabrica = crearFabrica();
+        const fila = fabrica.find(Fila).at(2);
+        // Es 3 ya que los indices de la columnas comienzan con 1
+        expect (fila.prop("index")).toEqual(3);
 
     })
-})
 
-/*
-describe('<Fabrica />', () => {
-    it('se monta como corresponde', () => {
-       let wrapper = shallow(<Fabrica />)
-  
-        expect(wrapper.find('.grilla')).toHaveLength(1)
-    })
-  
-    it('tiene 100 celdas', () => {
-       let wrapper = shallow(<Fabrica />)
-  
-        expect(wrapper.find(Fila)).toHaveLength(100)
-    })
+    /*
     
-  })
-  */
+    it("La cantidad de grillas creadas deberia ser 48",() =>{
+        const fabrica = crearFabrica();
+        fabrica.simulate('click')
+        // Da 0, preguntar despues bien porque no funciona
+        expect (fabrica.state("grillas").length).toEqual(48);
+
+    })
+
+    */
+})
