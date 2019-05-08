de058d9add5dcf1862725901c78c022f731e267b
Mari Medici <marilau.medici@gmail.com>
2019-05-07 23:15:04 -0300

    TO-DO

--- src/reducer/CeldaReducer.js
+++ src/reducer/CeldaReducer.js
@@ -1,11 +1,24 @@
 import { Id, Buttons } from '../types/ButtonType';
 import type { MaquinaAction } from '../types';
-
+import { mount } from 'enzyme';
+import  Celda  from '../components/Celda';
+import  Fabrica  from '../components/Fabrica';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const celdas = (state: Celdas = [], action: MaquinaAction): Celdas => {
+    if(state.length === 0 ){
+
+      //TODO llenameee de celdas
+
+      //ReactDOM.findDOMNode(<Fabrica/>).getElementsByClassName('celda');
+      //</instance-of-outermost-component>const celdas = mount(<Celda className="celda" />)
+      //return (boton === [] ? undefined : boton[0]);
+      //console.log(celdas);
+    }
     switch (action.type) {
       case 'AGREGAR_MAQUINA':
-        return state;
+        return state; //armar metodo apra agregar una maquina a una celda
       default:
         return state;
     }
