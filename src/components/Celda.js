073a044d08682c302391c896a45b45285d4a4884
Diego <garcia.diego1990@gmail.com>
2019-05-11 16:50:01 -0300

    fix 'src/index.js' y 'components/Celda.js'

--- src/components/Celda.js
+++ src/components/Celda.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+//import { PropTypes } from 'prop-types';
 import './Celda.css';
 
 export class Celda extends Component {
